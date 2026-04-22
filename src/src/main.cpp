a9899477384542a16ba092d72cb28651d8466778
domas-cloud <lukassdomass@gmail.com>
2026-04-22 21:06:21 +0300

    Add ESP32 control code and library headers

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -5,7 +5,7 @@
 #include <Compass.h>
 #include <ESP32Servo.h>
 #include <Wire.h>
-#include <Sorting.h>
+#include <Lidar.h>
 
 int BUTTON_PIN = 13;
 int BUTTON_STATE = 1;
@@ -22,10 +22,14 @@ auto engine = Engine(MOTOR_1, MOTOR_2, ENABLE_MOTOR);
 Compass robotCompass;
 Servo myservo;
 
+Distance_Sensor leftSensor;
+Distance_Sensor rightSensor;
+Distance_Sensor frontSensor;
+
 void setup() {
+  Serial.begin(115200);
+
   Wire.begin();
-  Wire.setClock(400000);
-  Serial.begin(9600);
 
   set_light_state(2, 0);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
@@ -35,45 +39,75 @@ void setup() {
   pinMode(MOTOR_2, OUTPUT);
   pinMode(ENABLE_MOTOR, OUTPUT);
 
+  // Reset
+  pinMode(5, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(15, OUTPUT);
+  digitalWrite(5, LOW);
+  digitalWrite(18, LOW);
+  digitalWrite(15, LOW);
+
+  delay(50);
+
+  if (!frontSensor.begin(15, 0x30)) {
+    Serial.println("Front ToF failed!");
+    while(1);
+  }
+
+  delay(50);
+  
+  if (!leftSensor.begin(5, 0x31)) {
+    Serial.println("Left ToF failed!");
+    while(1);
+  }
+
+  delay(50);
+
+  if (!rightSensor.begin(18, 0x32)) {
+    Serial.println("Right ToF failed!");
+    while(1);
+  }
+
+  if (!robotCompass.begin()) {
+    Serial.println("Compass failed!");
+    while(1);
+  }
+
   ledcSetup(0, 30000, 8);
   ledcAttachPin(ENABLE_MOTOR, 0);
 
-  setup_lidar_sensors();
   engine.begin();
 
-  // Initialize Compass
-  if (!robotCompass.begin()) {
-      Serial.println("BNO085 Fail!");
-  }
-
-  myservo.attach(33); 
+  myservo.attach(33, 500, 2400); 
   set_light_state(2, 3);
 };
 
-float started = false;
+bool started = false;
 float last_error = 0;
 float last_time = millis();
-const size_t buffer_size = 30;
-float track_buffer[buffer_size] = { 0 };
-float targetAngle = 0;
-const float STRAIGHT_ANGLE = 80.0;
-const float Kp = 0.02;
-const float Kd = 0;
-int track_tracker = 0;
+int targetAngle = 0;
+int edge = 0;
+bool isClockwise = false;
+
+const int MIN_ANGLE = 60;
+const int MAX_ANGLE = 120;
+const int STRAIGHT_ANGLE = 88;
+const int TARGET_DISTANCE = 300;
+const int TURN_DISTANCE = 400;
+
+const float Kp = 0.09; // 0.1
+const float Kg = 0.95;
+const float Kd = 0.05; // 0.1
 
 void loop() {
-
-    // Read and set sensor states
     BUTTON_STATE = digitalRead(BUTTON_PIN);
     blink_lights();
-    read_lidar_data();
 
     float newHeading = robotCompass.getYaw();
-
     if (BUTTON_STATE == 0) {
       started = !started;
       targetAngle = newHeading;
-      delay(100);
+      delay(400);
     } 
 
     if (!started) {
@@ -85,46 +119,77 @@ void loop() {
     set_light_state(2, 1);
     engine.drive(255);
 
-    float angle = targetAngle - newHeading;
-    if (angle > 180)  angle -= 360;
-    if (angle < -180) angle += 360;
+    int heading = targetAngle - newHeading;
+    if (heading > 180)  heading -= 360;
+    if (heading < -180) heading += 360;
 
-    float rad_angle = radians(angle);
-    float width = (SENSOR_DISTANCE[0] + SENSOR_DISTANCE[1]) * cos(rad_angle);
-        if (track_tracker == (buffer_size - 1)) {
-        track_tracker = 0;
+    Distance_Result frontDistance = frontSensor.measureDistance();
+    Distance_Result leftDistance = leftSensor.measureDistance();
+    const Distance_Result rightDistance = rightSensor.measureDistance();
+
+    // 2 or 4 when object is too far
+    
+    if (frontDistance.distance <= TURN_DISTANCE) {
+      isClockwise = leftDistance.distance <= 800 && leftDistance.status == 0;
+      while (frontDistance.distance <= 1000 || frontDistance.distance >= 2700) {
+        frontDistance = frontSensor.measureDistance();
+        //Serial.println(String("left:") + leftDistance.distance + " " + frontDistance.distance + " status: " + leftDistance.status);
+
+        if (isClockwise) {
+          myservo.write(MIN_ANGLE);
+        } else {
+          myservo.write(MAX_ANGLE);
+        }
+
+        delay(20);
+      }
+
+      if (isClockwise) {
+        targetAngle -= 90;
+      } else {
+        targetAngle += 90;
+      }
+
+      targetAngle %= 360;
+      edge++;
+      set_light_state(2, 3, edge);
+    }
+
+    float angle = Kg * heading;
+
+    //Serial.println(String("L: ") + leftDistance.distance + "mm (" + leftDistance.status + ") T: " 
+    //+ targetAngle + ") Gyro: " + heading + " Front: " + frontDistance.distance + "mm");
+
+    const Distance_Result outerDistance = isClockwise ? leftDistance : leftDistance;
+
+    // if the robot is going counterclockwise,  we should use the right sensor
+    const int dist_err = outerDistance.distance - TARGET_DISTANCE;
+    float err = heading - last_error;
+
+    if (outerDistance.distance <= 850 && outerDistance.distance >= 100) {
+      angle += Kp * dist_err;
+    }
+
+    if (last_error == 0) {
+      last_time = millis();
+      last_error = err;
     } else {
-        track_tracker++;
+      err /= (millis() - last_time);
+      last_time = millis();
+      angle -= Kd * err;
     }
 
-    track_buffer[track_tracker] = width;
-    const float track = get_dominant_cluster_average(
-        buffer_size, track_buffer, 20
-    );
-    float distance = SENSOR_DISTANCE[0] * cos(rad_angle);
+    if (abs(angle) < 3) {
+      angle = 0;
 
-    float error = (track) / 2 - distance; 
-    if (!last_error) {
-        last_error = error;
+      // Stop after the robot is centered in the sector
+      if (edge >= 12) {
+        engine.stop();
+        ESP.restart();
+      }
     }
 
-    unsigned long now = millis();
-    float delta_t = (now - last_time) / 1000.0f;
-    last_time = now;
-
-
-    const float derivative_delta = (error - last_error) / delta_t;
-    last_error = error;
-
-    int turning_angle = STRAIGHT_ANGLE 
-        + Kp * error
-        + Kd * derivative_delta;
-
-    //Serial.printf("Left: %.2f | Right: %.2f | Width: %.2f\n", SENSOR_DISTANCE[1], SENSOR_DISTANCE[0], width);
-
-    float final_servo_angle = constrain(turning_angle, STRAIGHT_ANGLE - 45, STRAIGHT_ANGLE + 45);
-    Serial.printf("Proportional: %.2f | Error: %.2f | Derivative: %.2f | Angle: %.2f | Width: %.2f \n", Kp * error, error, Kd * derivative_delta, final_servo_angle, track);
-
-    myservo.write(final_servo_angle);
+    const int angle_constrained = constrain(STRAIGHT_ANGLE + round(angle), MIN_ANGLE, MAX_ANGLE);
+    myservo.write(angle_constrained);
 };
 
